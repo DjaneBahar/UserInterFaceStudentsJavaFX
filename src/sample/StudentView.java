e979910402db689c0659b76662afd9614c27bb2f
DjaneBahar <baharsadik@hotmail.com>
2020-11-18 15:57:44 +0100

    combobox grades

--- src/sample/StudentView.java
+++ src/sample/StudentView.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.beans.Observable;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
@@ -20,6 +21,9 @@ public class StudentView {
     Label StudentsHeader = new Label("Find grades and courses taken, and the average grade for a selected student");
     Label StudentsLbl = new Label("Select Student: ");
 
+    Label ChooseGradeLbl = new Label("Insert missing grade for SD2020:");
+
+
     //Find average grade on a selected course
     Label CourseHeader = new Label("Find average grade on a selected course");
     Label SelectCourseLbl = new Label("Select a Course and Semester: ");
@@ -28,12 +32,14 @@ public class StudentView {
     Button avgCourseBtn = new Button("Show Average grade for course");
     Button showRstBtn = new Button("Show Results for student");
     Button exitBtn = new Button("Exit");
+    Button insertBtn = new Button("Insert Grade");
 
     TextArea StudentText = new TextArea();
     TextArea CourseText = new TextArea();
 
     ComboBox<String> StudentComB = new ComboBox<String>();
     ComboBox<String> CoursesComB = new ComboBox<String>();
+    ComboBox<Integer> GradesComB = new ComboBox<Integer>();
 
     public StudentView(StudentModel model, Controller control){
         this.model = model;
@@ -52,7 +58,10 @@ public class StudentView {
 
         //Print for Students
         Startview.add(StudentsHeader,1,1);
+
         Startview.add(StudentsLbl,1,3);
+        Startview.add(ChooseGradeLbl, 2,3);
+
 
         ObservableList<String>StudentList = control.getStudents();
         StudentComB.setItems(StudentList);
@@ -60,6 +69,15 @@ public class StudentView {
         Startview.add(StudentComB,1,5);
         Startview.add(showRstBtn,1,7);
         Startview.add(StudentText, 1,9,30,10);
+
+
+
+        ObservableList<Integer>GradesList = control.getSetGrades();
+        GradesComB.setItems(GradesList);
+        GradesComB.getSelectionModel().selectFirst();
+        Startview.add(GradesComB,2,5);
+
+        Startview.add(insertBtn,2,7);
 
         //Print for Courses
         Startview.add(CourseHeader,1,21);
